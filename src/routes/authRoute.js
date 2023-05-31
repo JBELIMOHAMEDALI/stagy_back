e1036375d07e71ceac5c788e57567d55a4885f61
khalil <khalil3draoui@gmail.com>
2023-05-31 08:55:22 +0100

    file fixed

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 const upload = require("../middlewares/fileMiddleware");
 
-router.post('/register', upload.single(file),authController.register);
+router.post('/register', upload.single("file"),authController.register);
 router.post('/login', authController.login);
 module.exports = router;
